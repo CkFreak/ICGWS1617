c7a68c208d295eb1d53b0b427d140edf1eb7417c
l3visan <thomas.hfm@googlemail.com>
2016-11-05 23:58:36 +0100

    waaaas geht hier ab!?

--- Pacman/WebGLExample/01-Triangle/triangle.js
+++ Pacman/WebGLExample/01-Triangle/triangle.js
@@ -1,10 +1,9 @@
 var gl;
-var generateCustomPacman;
 
-window.onload = function init()
+window.onload = function init(vertices, angle, radius)
 {
 	// Get canvas and setup WebGL
-    var array2 =  new Float32Array(drawPacman(502,60,10));
+    var array2 =  new Float32Array(drawPacman(vertices, angle, radius));
     var colors = new Float32Array(drawColor(array2.length / 2));
 
     console.log(array2.length)
@@ -17,65 +16,62 @@ window.onload = function init()
 	if (!gl) { alert("WebGL isn't available"); }
 
 	// Specify position and color of the vertices
-	// Array mit den Koordinaten
-	var vertices;
-	// Array mit den Farben
-	var colors;
+    function toRadians(angle)
+    {
+        return (angle * Math.PI / 180);
+    }
 
-	/*Diese Funktion nimmt einen Radius zwischen 0 und 1 eine Anzahl an vertices und einen Winkel für den Mund
-	  Das Array vertices und colors wird mit den passenden Werten gefüllt
-		Aus Gründen wie der Kreis aufgebaut wird wird erst bei etwa 150 Dreiecken der Mund in der angegebenen Größe angezeigt
-	*/
-	function drawPacman(r, nOV, mAngle)
-	{
-		//Hier wird aus der nOV und dem mAngle der angle berechnet
-    //Der angle repräsentiert wie viele Dreiecke aus dem Kreis entfehrnt werden um den Mund freizulegen
-		var anglePT = (360 / (nOV - 2)); //Der Winkel den jedes Dreiecke hat
-		var angle = (mAngle / anglePT).toFixed(0); //Anzahl der zu entfehrnenden Dreiecke
-		if(angle % 2 != 0){ //Wenn die Anzahl der zu enfernenden Dreiecke nicht grade ist wird sie grade gemacht
-			angle++;
-		}
 
-		verticesx = new Float32Array((nOV - angle)); //Hält die x Koordinaten. Die Koordinaten für die zu entfehrnenden Dreiecke werden abgezogen
-		verticesy = new Float32Array((nOV - angle)); //Hält die y Koordinaten. Die Koordinaten für die zu entfehrnenden Dreiecke werden abgezogen
-		colors = new Float32Array(nOV * 4); //Hält die Farben für die Dreiecke
-		verticesx[0] = 0; //Die ersten Punkte setzen den Mittelpunkt des Kreises
-		verticesy[0] = 0; //Hält die x Koordinaten. Die Koordinaten für die zu entfehrnenden Dreiecke werden abgezogen
+    function drawPacman(vertices, angle, radius)
+    {
+        var rad = 0.1* radius;
+        var winkelEck = toRadians(360 /vertices );
+        var array =[0,0];
+        var entfTri = 0;
+        var angleR = toRadians(angle);
+        if (vertices < 16)
+            {
+                if (angle <= 45)
+                    {
+                        var anfangsK = winkelEck / 2;
+                    }
+                else
+                {
+                    var anfangsK = angleR / 2;
+                }
+                
+            }
+        else
+            {
+                var anfangsK = angleR / 2;
+            }
 
-		//Befüllt verticesx und verticesy mit Koordinaten. Legt man durch den Pacman eine Horizontale Linie
-		//so werden ober und unterhalb von ihr die Koordinaten für die Dreiecke die den Mund aussparen gleichmäßig ausgelassen
-		for (var i = 1; i < nOV; i++) {
-			if(i > angle / 2 && i < verticesx.length + (angle/2)){
-				verticesx[i - (angle / 2)] = r * Math.cos(i * 2 * Math.PI / (nOV - 2)); //In Bogenmaß + (anglePT / 2)
-				verticesy[i - (angle / 2)] = r * Math.sin(i * 2 * Math.PI / (nOV - 2));
-			}
-		}
+        
+        while (angleR >= winkelEck)
+            {
+                angleR = angleR - winkelEck;
+                entfTri++;
+            }
+        for (var i = 0; i<= (vertices - entfTri); i++)
+            {
+                array.push(rad * (Math.cos(winkelEck *i + anfangsK )));
+                array.push(rad * (Math.sin(winkelEck * i + anfangsK)));
+            }
 
-		verticesx[verticesx.length - 1] = 0; // Die letzte Koordinate zeigt wieder auf den Mittelpunkt
-		verticesy[verticesy.length - 1] = 0; // Die letzte Koordinate zeigt wieder auf den Mittelpunkt
 
-		vertices = new Float32Array((nOV - angle) * 2); //Array in dem die x und y Koordinaten vereint werden
-		for(var i = 0; i < verticesx.length; i++){
-			vertices[i * 2] = verticesx[i];
-			vertices[i*2 +1] = verticesy[i];
-		}
+        console.log(entfTri)
+        return array;
+    }
 
-		//Für jedes Dreieck wird eine Farbe hinzugefügt
-		for(var i = 0; i < verticesx.length * 4; i++){
-			colors[i*4] = 1;
-			colors[i*4 + 1] = 1;
-			colors[i*4 + 2] = 0;
-			colors[i*4 + 3] = 1;
-		}
-		//Zusatzinfos für die Konsole
-		console.log("Angegebener Radius: " + r);
-		console.log("Anzahl der vertices: " + nOV);
-		console.log("Anzahl der Dreiecke gesamt: " + (nOV - 2));
-		console.log("Angegebener Winkel: " + mAngle + "°");
-		console.log("Anzahl der zu entfehrnenden Dreiecke: " + angle);
-	}
-
-	drawPacman(1, 10, 30);
+    function drawColor(vertices)
+    {
+        var colors =[];
+        for (var i = 0; i < (vertices); i++)
+            {
+                colors.push(1,1,0,1);
+            }
+        return colors;
+    }
 
 	// Configure viewport
 
@@ -91,7 +87,7 @@ window.onload = function init()
 
 	var posVBO = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, posVBO);
-	gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+	gl.bufferData(gl.ARRAY_BUFFER, array2, gl.STATIC_DRAW);
 
 	var vPosition = gl.getAttribLocation(program, "vPosition");
 	gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
@@ -107,7 +103,7 @@ window.onload = function init()
 	gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(vColor);
 
-	render((array2.length / 2));
+	render(array2.length / 2);
 
   generateCustomPacman = function ()
   {
@@ -115,13 +111,13 @@ window.onload = function init()
     var angle = document.getElementById("angle").value;
     var radius = document.getElementById("radius").value;
 
-    render(drawPacman(vertices, angle, radius).length/2);
+    init(vertices, angle, radius);
   };
+
 };
 
-//Da sich je nach Angaben die länge des Arrays ändert wird dies mit übergeben
-function render(vertices)
+function render(lengths)
 {
 	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length / 2);
+	gl.drawArrays(gl.TRIANGLE_FAN, 0, lengths);
 }
