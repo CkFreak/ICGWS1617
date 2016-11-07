23acfba3976ede4a73b62048be5117bc42cc6e8b
FungiPenny <mrfungipenny@gmail.com>
2016-11-07 01:13:17 +0100

    Aufgabe 1 fertig

--- josh/WebGLExample/01-Triangle/triangle.js
+++ josh/WebGLExample/01-Triangle/triangle.js
@@ -2,6 +2,7 @@ var gl;
 
 window.onload = function init()
 {
+
 	// Get canvas and setup WebGL
 
 	var canvas = document.getElementById("gl-canvas");
@@ -9,32 +10,46 @@ window.onload = function init()
 	if (!gl) { alert("WebGL isn't available"); }
 
 	// Specify position and color of the vertices
-	/*
-	var vertices = new Float32Array([	-1, -1,
-										0, 1,
-										1, -1]);
-	var colors = new Float32Array([ 1, 0, 0, 1,
-									1, 1, 0, 1,
-									0, 0, 1, 1,]);
+
+	var vertices = new Float32Array(getVertices(360, 90, 5));
+	var colors = new Float32Array(getColors(vertices.length / 2));
+
+	function toRadians(angle){
+		return (angle * Math.PI / 180);
+	}
+
+	/**
+	*	Gibt das Vertices-Array zurück und nimmt dafür die
+	* Anzahl der Vertices, den Winkel und die Größe des Pacmans entgegen.
 	*/
-	//TODO: Methodenaufrufe die vertices und color Werte zuweisen
-  //var vertices = new Float32Array([]);
-	//var colors = new Float32Array([])
-	// Configure viewport
+	function getVertices(numberOfVertices, mouthAngle, size){
+		var verticesArray = [0,0];
 
-	function getVertices(){
-		return new Float32Array([ 1, 0, 0, 1,
-			1, 1, 0, 1,
-			0, 0, 1, 1,]);
+		var vangle = toRadians(360 / numberOfVertices);
+		var mouth = toRadians(mouthAngle) / vangle;
 
+		var radius = (size % 10) / 10;
+
+		for(var i = 0; i <= numberOfVertices - mouth; i++){
+			verticesArray.push(radius * (Math.cos((i + mouth/2) * vangle)));
+			verticesArray.push(radius * (Math.sin((i + mouth/2) * vangle)));
+		}
+		return verticesArray;
+
+	}
+
+	function getColors(number){
+		var colorArray = [];
+
+		for(var i = 0; i < number; i++){
+			colorArray.push(0.5, 1, 0.25, 1);
 		}
 
-		function getColors(){
-			return new Float32Array([ 1, 0, 0, 1,
-				1, 1, 0, 1,
-				0, 0, 1, 1,]);
-			}
+		return colorArray;
+	}
 
+
+	// Configure viewport
 	gl.viewport(0,0,canvas.width,canvas.height);
 	gl.clearColor(1.0,1.0,1.0,1.0);
 
@@ -44,33 +59,28 @@ window.onload = function init()
 	gl.useProgram(program);
 
 	// Load positions into the GPU and associate shader variables
-  //TODO: Zugriff auf Methode, die vertices verändert oder returnt
 	var posVBO = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, posVBO);
-	gl.bufferData(gl.ARRAY_BUFFER, vertices = getVertices(), gl.STATIC_DRAW);
+	gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
 	var vPosition = gl.getAttribLocation(program, "vPosition");
 	gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(vPosition);
 
 	// Load colors into the GPU and associate shader variables
-  //TODO: Zugriff auf Methode, die colors verändert oder returnt
 	var colorVBO = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, colorVBO);
-	gl.bufferData(gl.ARRAY_BUFFER, color = getColors(), gl.STATIC_DRAW);
+	gl.bufferData(gl.ARRAY_BUFFER, colors, gl.STATIC_DRAW);
 
 	var vColor = gl.getAttribLocation(program, "vColor");
 	gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(vColor);
 
-	render();
+	render(vertices.length / 2);
 };
 
-//TODO: Anzahl der Vertices durch Variable ersetzen
-function render()
+function render(length)
 {
 	gl.clear(gl.COLOR_BUFFER_BIT);
-	gl.drawArrays(gl.TRIANGLES_FAN, 0, 3);
+	gl.drawArrays(gl.TRIANGLE_FAN, 0, length);
 }
-
-//TODO: Hilfsfunktionen, die Arrays returnen oder verändern
